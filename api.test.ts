d9d489143a4f735146ca1d67fef178acb2d282e0
Arthur Chumak <arthurchumak@gmail.com>
2019-02-18 17:20:23 +0300

    Update api.test.ts

--- api.test.ts
+++ api.test.ts
@@ -3,11 +3,9 @@ import {GitHub} from "./api";
 describe("GitHub", () => {
     const gitHub = new GitHub("https://api.github.com/");
         
-    test("getting Profile by username", async (done) => {
+    test("getting Profile by username", async () => {
         const response = await gitHub.getProfile("arthurchumak");
 
         expect(response.status).toBe(200);
-
-        done();
     });
 });
